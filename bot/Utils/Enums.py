cfa3009bdbb7369258628f42e197b70ce95a866d
Adam Buchowski <adam.buchowski@proton.me>
2026-06-15 12:42:11 +0200

    moved enums to separate Utils module

---
+++ bot/Utils/Enums.py
@@ -0,0 +1,26 @@
+RARITY_COLOR = {
+    "Common": 0xfdfefe,
+    "Uncommon": 0x27ae60,
+    "Rare": 0x2471a3,
+    "Epic": 0x7d3c98,
+    "Legendary": 0xf1c40f,
+    "Mythic": 0xd35400
+}
+
+RARITY_WEIGHT = {
+    "Common": 7000,
+    "Uncommon": 2000,
+    "Rare": 700,
+    "Epic": 250,
+    "Legendary": 40,
+    "Mythic": 10
+}
+
+RARITY_EMOJI = {
+    "Common": "👎",
+    "Uncommon": "👍",
+    "Rare": "🤙",
+    "Epic": "😎",
+    "Legendary": "🥵",
+    "Mythic": "🔥"
+}
