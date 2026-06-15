81c44c590ad2d3f03f540c6890757930a4186003
Adam Buchowski <adam.buchowski@proton.me>
2026-06-15 12:42:49 +0200

    Updated card class and added support for dicord components

--- bot/Data/Card.py
+++ bot/Data/Card.py
@@ -1,17 +1,37 @@
+from bot.Utils.Enums import RARITY_COLOR, RARITY_EMOJI
 import discord
+from discord.ext import commands
 
 class Card():
     def __init__(self, name="None", description="None", bundle="None", collection="None", rarity="Common", image_url="https://i.imgur.com/izgPuO1.jpeg"):
         self.name = name
-        self.description = description
         self.bundle = bundle
         self.collection = collection
+        self.description = description
         self.rarity = rarity
         self.image_url = image_url
 
-    def to_embed(self):
-        embed = discord.Embed()
-        embed.title = self.name
-        embed.description = f"**Bundle:** {self.bundle}\n**Collection:** {self.collection}\n**Rarity:** {self.rarity}\n**Description:** {self.description}"
-        embed.set_image(url=self.image_url)
-        return embed
+    @classmethod
+    def from_json(cls, data:dict, bundle_name:str, collection_name:str):
+        return cls(
+            name = data["name"],
+            bundle = bundle_name,
+            collection = collection_name,
+            rarity = data["rarity"],
+            description = data["description"],
+            image_url = data["image_url"]
+        )
+
+    def to_container(self):
+        container = discord.ui.Container(
+            discord.ui.TextDisplay(content=f"{RARITY_EMOJI[self.rarity]} **{self.rarity}** {self.name} {RARITY_EMOJI[self.rarity]}"),
+            discord.ui.TextDisplay(content=f"**Bundle:** {self.bundle}\n**Collection:** {self.collection}\n{self.description}"),
+            discord.ui.MediaGallery(
+                discord.MediaGalleryItem(
+                    media=self.image_url
+                )
+            ),
+            accent_color=RARITY_COLOR[self.rarity]
+        )
+
+        return container
