84237524324857e88b6d3f9f12752faabb6ea1f1
Adam Buchowski <adam.buchowski@proton.me>
2026-06-16 14:32:42 +0200

    Updated views

--- bot/Views/PageView.py
+++ bot/Views/PageView.py
@@ -2,15 +2,25 @@ import discord
 from discord.ext import commands
 
 class PageView(discord.ui.LayoutView):
-    def __init__(self, pages: list[discord.ui.Container], author_id: int, header: str):
+    def __init__(self, pages: list[discord.ui.Container], author_id: int, header: str|None=None, thumbnail: str|None=None):
         super().__init__()
 
         self.pages = pages
         self.author_id = author_id
         self.current_page = 0
+        self.header = None
 
         # Header
-        self.header = discord.ui.TextDisplay(content=header)
+        if header:
+            if thumbnail:
+                self.header = discord.ui.Container(
+                    discord.ui.Section(
+                        discord.ui.TextDisplay(content=header),
+                        accessory=discord.ui.Thumbnail(media=thumbnail)
+                    )
+                )
+            else:
+                self.header = discord.ui.Container(discord.ui.TextDisplay(content=header))
 
         # Container
         self.container = self.pages[self.current_page]
@@ -33,7 +43,7 @@ class PageView(discord.ui.LayoutView):
         self.actions = discord.ui.ActionRow(self.prev_btn, self.next_btn)
 
         # Footer
-        self.footer = discord.ui.TextDisplay(content=f"({self.current_page+1}/{len(self.pages)})")
+        self.footer = discord.ui.TextDisplay(content=f"Page {self.current_page+1}/{len(self.pages)}")
 
         self.update_view()
 
@@ -49,11 +59,12 @@ class PageView(discord.ui.LayoutView):
         self.container = self.pages[self.current_page]
 
         # Update footer
-        self.footer = discord.ui.TextDisplay(content=f"({self.current_page+1}/{len(self.pages)})")
+        self.footer = discord.ui.TextDisplay(content=f"Page {self.current_page+1}/{len(self.pages)}")
 
         # Update view
         self.clear_items()
-        self.add_item(self.header)
+        if self.header:
+            self.add_item(self.header)
         self.add_item(self.container)
         self.add_item(self.footer)
         self.add_item(self.actions)
---
+++ bot/Views/SimpleView.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext import commands
+
+class SimpleView(discord.ui.LayoutView):
+    def __init__(self, content: discord.ui.Container, header: str|None=None, thumbnail: str|None=None):
+        super().__init__()
+        
+        # Header
+        self.header = None
+        if header:
+            if thumbnail:
+                self.header = discord.ui.Container(
+                    discord.ui.Section(
+                        discord.ui.TextDisplay(content=header),
+                        accessory=discord.ui.Thumbnail(media=thumbnail)
+                    )
+                )
+            else:
+                self.header = discord.ui.Container(discord.ui.TextDisplay(content=header))
+
+        self.container = content
+        if self.header:
+            self.add_item(self.header)
+        self.add_item(self.container)
