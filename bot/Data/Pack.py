e65f3b1ce1b8caf48d1f6a22cef87b3f9b4a4317
Adam Buchowski <adam.buchowski@proton.me>
2026-06-16 14:34:22 +0200

    Removed data classes in favor of simple dictionaries

--- bot/Data/Pack.py
+++
@@ -1,7 +0,0 @@
-from bot.Utils.Enums import RARITY_WEIGHT
-import random
-
-class Pack:
-    def __init__(self, name:str, card_names):
-        self.name = name
-        self.card_names = card_names
