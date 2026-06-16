399ea111dea3f353e15f684032708e20a5d8b071
Adam Buchowski <adam.buchowski@proton.me>
2026-06-16 14:35:02 +0200

    Moved commands to separate cogs

--- bot/Cogs/General.py
+++ bot/Cogs/General.py
@@ -2,58 +2,35 @@ import random
 
 import discord
 from discord.ext import commands
-
-# Data
-from bot.Data.Card import Card
-from bot.Data.Pack import Pack
-from bot.Data.User import User
+from discord import app_commands
 
 # Views
 from bot.Views.PageView import PageView
 
 # Utils
 from bot.Utils.Enums import RARITY_WEIGHT
+from bot.Utils.DataDriver import DataDriver
 
 class General(commands.Cog):
-    def __init__(self, bot):
+    def __init__(self, bot, datadriver:DataDriver):
         self.bot = bot
+        self.datadriver = datadriver
 
-    @discord.app_commands.command(name="test", description="A test command")
+    @app_commands.command(name="test", description="A test command")
     async def test(self, interaction:discord.Interaction):
         cards = self.bot.datadriver.get_all_cards()
         pages = [card.to_container() for card in cards]
         view = PageView(pages, interaction.user.id, "All cards")
         await interaction.response.send_message(view=view)
 
-    @discord.app_commands.command(name="lesgo", description="A test command")
+    @app_commands.command(name="lesgo", description="A test command")
     async def lesgo(self, interaction:discord.Interaction):
-        if self.bot.datadriver.user_exist(interaction.user.id):
+        if self.datadriver.user_exist(interaction.user.id):
             await interaction.response.send_message("User already exist.")
             return
         
-        self.bot.datadriver.create_user(user_id=interaction.user.id)
+        self.datadriver.create_user(user_id=interaction.user.id)
         await interaction.response.send_message("Your profile has been created.")
 
-    @discord.app_commands.command(name="profile", description="A test command")
-    async def profile(self, interaction:discord.Interaction):
-        pass
-
-    @discord.app_commands.command(name="open", description="Open a pack")
-    async def open_pack(self, interaction:discord.Interaction, pack_name:str):
-        pack = self.bot.datadriver.get_pack_by_name(pack_name)
-        if pack is None:
-            await interaction.response.send_message("Pack not found")
-            return
-
-        card_names = pack.card_names
-        cards = self.bot.datadriver.get_cards_from_list(card_names)
-
-        weights = [RARITY_WEIGHT[c.rarity] for c in cards]
-        cards = random.choices(cards, weights=weights, k=5)
-
-        pages = [card.to_container() for card in cards]
-        view = PageView(pages, interaction.user.id, f"You've opened {pack.name}")
-        await interaction.response.send_message(view=view)
-
 async def setup(bot):
-    await bot.add_cog(General(bot))
+    await bot.add_cog(General(bot, bot.datadriver))
