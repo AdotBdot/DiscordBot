be37c64f98b242a475ac483f60985bbc9d42ccf8
Adam Buchowski <adam.buchowski@proton.me>
2026-06-16 14:37:24 +0200

    removed reduntant comments

--- bot/Utils/Permissions.py
+++ bot/Utils/Permissions.py
@@ -3,12 +3,12 @@ from discord import app_commands
 
 def admin_only():
     async def predicate(interaction: discord.Interaction):
-        return interaction.user.guild_permissions.administrator
+        return interaction.user.guild_permissions.administrator # type: ignore
     
     return app_commands.check(predicate)
 
 def owner_only():
     async def predicate(interaction: discord.Interaction):
-        return interaction.user.id == interaction.guild.owner_id
+        return interaction.user.id == interaction.guild.owner_id # type: ignore
     
     return app_commands.check(predicate)
