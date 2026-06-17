8f3ab65c9146be4dcb85cd7441194f1fac4e5028
Adam Buchowski <adam.buchowski@proton.me>
2026-06-17 12:33:52 +0200

    Removed test command. /help command template

--- bot/Cogs/General.py
+++ bot/Cogs/General.py
@@ -12,18 +12,11 @@ from bot.Utils.Enums import RARITY_WEIGHT
 from bot.Utils.DataDriver import DataDriver
 
 class General(commands.Cog):
-    def __init__(self, bot, datadriver:DataDriver):
+    def __init__(self, bot, datadriver: DataDriver):
         self.bot = bot
         self.datadriver = datadriver
 
-    @app_commands.command(name="test", description="A test command")
-    async def test(self, interaction:discord.Interaction):
-        cards = self.bot.datadriver.get_all_cards()
-        pages = [card.to_container() for card in cards]
-        view = PageView(pages, interaction.user.id, "All cards")
-        await interaction.response.send_message(view=view)
-
-    @app_commands.command(name="lesgo", description="A test command")
+    @app_commands.command(name="lesgo", description="Creates user profile")
     async def lesgo(self, interaction:discord.Interaction):
         if self.datadriver.user_exist(interaction.user.id):
             await interaction.response.send_message("User already exist.")
@@ -31,6 +24,10 @@ class General(commands.Cog):
         
         self.datadriver.create_user(user_id=interaction.user.id)
         await interaction.response.send_message("Your profile has been created.")
+
+    @app_commands.command(name="help", description="Displays help.")
+    async def help(self, interaction: discord.Interaction):
+        pass
 
 async def setup(bot):
     await bot.add_cog(General(bot, bot.datadriver))
