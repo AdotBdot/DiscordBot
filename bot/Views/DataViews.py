39a42db2ab373e48ffad42259dae032302a04d92
Adam Buchowski <adam.buchowski@proton.me>
2026-06-17 12:31:19 +0200

    Added text wraping for descriptions and adjusted user container generation

--- bot/Views/DataViews.py
+++ bot/Views/DataViews.py
@@ -2,24 +2,35 @@ import discord
 from discord.ext import commands
 
 from bot.Utils.Enums import RARITY_COLOR, RARITY_EMOJI
+from bot.Utils.Helpers import wrap_text
 
 def card_to_container(card: dict) -> discord.ui.Container:
     rarity = card["rarity"]
+    description = "\n".join(wrap_text("**Description**: " + card["description"]))
+
+    tags = ""
+    if card["tags"]:
+        tags = ", ".join([tag for tag in card["tags"]])
+    else: 
+        tags = "None" 
+
     container = discord.ui.Container(
-            discord.ui.TextDisplay(content=f"{RARITY_EMOJI[rarity]} **{rarity}** {card["name"]} {RARITY_EMOJI[rarity]}"),
-            discord.ui.TextDisplay(content=f"**Bundle:** {card["bundle"]}\n**Collection:** {card["collection"]}\n**Tags**: {", ".join([tag for tag in card["tags"]])}\n{card["description"]}"),
+            discord.ui.TextDisplay(content=f"## {card["name"]}"),
+            discord.ui.TextDisplay(content=f"**Rarity**: {rarity} {RARITY_EMOJI[rarity]}\n**Bundle:** {card["bundle"]}\n**Collection:** {card["collection"]}\n**Tags**: {tags}"),
             discord.ui.MediaGallery(
                 discord.MediaGalleryItem(media=card["image_url"])
             ),
+            discord.ui.TextDisplay(content=description),
             accent_color=RARITY_COLOR[card["rarity"]]
         )
     
     return container
 
-def user_to_container(user: dict, avatar_url: str, total_cards: int) -> discord.ui.Container:
+def user_to_container(user: dict, total_cards: int) -> discord.ui.Container:
     container = discord.ui.Container(
-            discord.ui.TextDisplay(content=f"**Collection Size**: {len(user["cards"])}\n**Completion**: {len(set(user["cards"]))}/{total_cards}"),
-            discord.ui.TextDisplay(content=f"**Cocoses**: {user["cash"]} 🥥\n**Melones**: {user["melons"]} 🍉")
+            discord.ui.TextDisplay(content=f"### Collection\n**Size**: {len(user["cards"])}\n**Completion**: {len(set(user["cards"]))}/{total_cards}"),
+            discord.ui.Separator(visible=True, spacing=discord.SeparatorSpacing.small),
+            discord.ui.TextDisplay(content=f"### Currency\n**Cocoses**: {user["cash"]} 🥥\n**Melones**: {user["melons"]} 🍉")
         )
 
     return container
