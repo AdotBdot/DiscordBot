f9f10a00b07b4b7f0837d5af3e4a43d9c33bb43d
Adam Buchowski <adam.buchowski@proton.me>
2026-06-17 12:33:25 +0200

    Added /stats and basic /give commands

--- bot/Cogs/Moderation.py
+++ bot/Cogs/Moderation.py
@@ -6,10 +6,26 @@ from discord import app_commands
 from bot.Utils.Permissions import admin_only, owner_only
 from bot.Utils.DataDriver import DataDriver
 
+from bot.Views.SimpleView import SimpleView
+
 class Moderation(commands.Cog):
     def __init__(self, bot, datadriver: DataDriver):
         self.bot = bot
         self.datadriver = datadriver
+        
+    # ====================
+    # Autocomplete
+    # ====================
+
+    async def pack_autocomplete(self, interaction: discord.Interaction, current:str) -> list[app_commands.Choice[str]]:
+        packs = self.datadriver.packs_cache
+        choices = [app_commands.Choice(name=pack, value=pack) for pack in packs if current.lower() in pack.lower()]
+        
+        return choices[:25]
+
+    # ====================
+    # General commands
+    # ====================
 
     # TODO: Implement setup command
     @app_commands.command(name="setup", description="Setups bot on server.")
@@ -42,6 +58,76 @@ class Moderation(commands.Cog):
     async def uptime(self, interaction: discord.Interaction):
         await interaction.response.send_message(f"{self.bot.uptime}")
 
+    @app_commands.command(name="stats", description="Displays bot stats.")
+    @admin_only()
+    async def status(self, interaction: discord.Interaction):
+        total_seconds = int(self.bot.uptime.total_seconds())
+
+        days, remainder = divmod(total_seconds, 86400)
+        hours, remainder = divmod(remainder, 3600)
+        minutes, seconds = divmod(remainder, 60)
+
+        container = discord.ui.Container(
+            discord.ui.TextDisplay(content=f"**Uptime**: {days}d {hours}h {minutes}m {seconds}s"),
+            discord.ui.Separator(visible=True),
+            discord.ui.TextDisplay(content=f"**Users**: {len(self.datadriver.users)}"),
+            discord.ui.TextDisplay(content=f"**Bundles**: {len(self.datadriver.bundle_cache)}\n**Collections**: {len(self.datadriver.collection_cache)}\n**Cards**: {self.datadriver.get_cards_count()}\n**Packs**: {len(self.datadriver.packs)}")
+        )
+
+        view = SimpleView(content=container, header="## Bot stats")
+        await interaction.response.send_message(view=view)
+
+    # ====================
+    # Give commands
+    # ====================
+
+    give = app_commands.Group(name="give", description="Give item to user inventory.")
+
+    @give.command(name="pack")
+    @admin_only()
+    @app_commands.autocomplete(pack=pack_autocomplete)
+    async def give_pack(self, interaction: discord.Interaction, target_user: discord.Member, pack: str, count: int):
+        user = self.datadriver.get_user(target_user.id)
+
+        if user is None:
+            await interaction.response.send_message(f"User does not exist in database.")
+            return
+        
+        for _ in range(count):
+            user["packs"].append(pack)
+
+        self.datadriver.save_user(user["id"])
+        
+        await interaction.response.send_message(f"Gave {count} pack(s): {pack} to user {target_user.mention}")
+
+    @give.command(name="cocoses")
+    @admin_only()
+    async def give_cocoses(self, interaction: discord.Interaction, target_user: discord.Member, cocoses: int):
+        user = self.datadriver.get_user(target_user.id)
+
+        if user is None:
+            await interaction.response.send_message(f"User does not exist in database.")
+            return
+        
+        user["cash"] += cocoses
+        self.datadriver.save_user(user["id"])
+        
+        await interaction.response.send_message(f"Gave {cocoses} 🥥 to user {target_user.mention}")
+
+    @give.command(name="melones")
+    @admin_only()
+    async def give_melones(self, interaction: discord.Interaction, target_user: discord.Member, melones: int):
+        user = self.datadriver.get_user(target_user.id)
+
+        if user is None:
+            await interaction.response.send_message(f"User does not exist in database.")
+            return
+        
+        user["melons"] += melones
+        self.datadriver.save_user(user["id"])
+        
+        await interaction.response.send_message(f"Gave {melones} 🍉 to user {target_user.mention}")
+    
     # /config group
     # config = discord.app_commands.Group(name="config", description="Bot configuration commands.")
 
