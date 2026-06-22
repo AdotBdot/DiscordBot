0c96cd116e367db8042abde9c2faa85cb7e98669
Adam Buchowski <adam.buchowski@proton.me>
2026-06-22 15:06:14 +0200

    Don't display count for single cards

--- bot/Cogs/Users.py
+++ bot/Cogs/Users.py
@@ -232,7 +232,7 @@ class Users(commands.Cog):
             chunk = df.iloc[i:i + 20]
 
             msg = "\n".join(
-                f"{RARITY_EMOJI[row["rarity"]]} {key} **x{df_counts[key]}**"  # type: ignore
+                f"{RARITY_EMOJI[row["rarity"]]} {key} **x{df_counts[key]}**" if df_counts[key] > 1 else f"{RARITY_EMOJI[row["rarity"]]} {key}" # type: ignore
                 for key, row in chunk.iterrows()
             )
 
